f7ae7c8ec41493ab82902e46014a1ca9592f1318
Hengkiardo <hengki.sihombing@gmail.com>
2015-02-13 13:40:58 +0700

    API to update and delete user

--- routes.js
+++ routes.js
@@ -3,11 +3,11 @@ var Users     = require('./models/users');
 
 Route.get('/users', function (req, res) {
 
-	Users.find(function (err, users) {
-		if (err) return res.status(500).json(err);
+  Users.find(function (err, users) {
+    if (err) return res.status(500).json(err);
 
-		return res.status(200).json(users)
-	});
+    return res.status(200).json(users)
+  });
 })
 
 Route.post('/users', function (req, res) {
@@ -39,10 +39,32 @@ Route.get('/users/:user_id', function (req, res) {
 })
 
 Route.put('/users/:user_id', function (req, res) {
+  var userId = req.params.user_id
 
+  var dataToUpdate = req.body
+
+  Users.findByIdAndUpdate(userId, dataToUpdate, function (err, user) {
+    if (err) return res.status(500).json(err);
+
+    if (user) return res.status(200).json({status: 'success', 'message' : 'User has deleted', data: user})
+
+    return res.status(400).json({'status': 'error', 'message': 'User not found!' })
+
+  });
 })
 
 Route.delete('/users/:user_id', function (req, res) {
+
+  var userId = req.params.user_id
+
+  Users.findByIdAndRemove(userId, function (err, user) {
+    if (err) return res.status(500).json(err);
+
+    if (user) return res.status(200).json({status: 'success', 'message' : 'User has deleted'})
+
+    return res.status(400).json({'status': 'error', 'message': 'User not found!' })
+
+  });
 
 })
 
