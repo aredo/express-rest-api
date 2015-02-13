2644cfc81a83090e51c37bc01344cbdb915fa280
Hengkiardo <hengki.sihombing@gmail.com>
2015-02-13 11:06:01 +0700

    Express Basic routing

--- server.js
+++ server.js
@@ -5,6 +5,26 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+// respond with "Hello World!" on the homepage
+app.get('/user', function (req, res) {
+  res.send('Hello World!');
+})
+
+// accept POST request on the homepage
+app.post('/user', function (req, res) {
+  res.send('Got a POST request');
+})
+
+// accept PUT request at /user
+app.put('/user', function (req, res) {
+  res.send('Got a PUT request at /user');
+})
+
+// accept DELETE request at /user
+app.delete('/user', function (req, res) {
+  res.send('Got a DELETE request at /user');
+})
+
 var server = app.listen(3000, function () {
 
   var host = server.address().address
